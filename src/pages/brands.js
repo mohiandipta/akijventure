b3ce6fbb40f9dd57b68d6fa54f44629262d97c45
mohiandipta <mohianmostofa.com>
2022-02-27 18:03:16 +0600

    last commit

--- src/pages/brands.js
+++ src/pages/brands.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
 import clearup from '.././assets/products/clear-up.png'
 import clemon from '.././assets/products/Clemon-250-ml-PET-1.png'
+import frutika from '.././assets/products/FRUTIKA-_-MANGO-_-250G-1 (1).png'
+import mojo from '.././assets/products/mojo-can.png'
 
 const Brands = () => {
     return (
         <div className='brands' id='brands'>
+            <h3>Brands</h3>
             <div className='products'>
                 <div className='products-card'>
-                    <img src={clearup} alt='' />
+                    <img src={clearup} alt='clearup' />
+                </div>
+                <div className='products-card'>
+                    <img src={clemon} alt='clemon' />
+                </div>
+                <div className='products-card'>
+                    <img src={frutika} alt='frutika' />
+                </div>
+                <div className='products-card'>
+                    <img src={mojo} alt='mojo' />
                 </div>
             </div>
         </div>
